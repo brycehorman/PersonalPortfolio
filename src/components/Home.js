67d8a58cfbf2741bcae5b8baaf8dfa4f9b9a43c1
Bryce Horman <brycehorman@MBP14inch2021.fios-router.home>
2022-02-03 22:42:43 -0500

    testing

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export default function Home(){
     return(
         <div className="min-h-screen bg-blue-100 p-12">
             <div className="grid place-items-center">
-                <div className="text-6xl font-effect-emboss">Bryce Test Horman</div>
+                <div className="text-6xl font-effect-emboss">Bryce Horman</div>
                
                 <div><img class="image imageCircle" src={headshot}></img></div>
             </div>
